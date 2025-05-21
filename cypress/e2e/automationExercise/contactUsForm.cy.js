2854588246c8d071222b1d82c8c002fe6a029a75
Moysin94 <shekhmoysin786@gmail.com>
2025-05-21 17:43:33 +0530

    fresh commit after restored

--- cypress/e2e/automationExercise/contactUsForm.cy.js
+++ cypress/e2e/automationExercise/contactUsForm.cy.js
@@ -1,31 +1,18 @@
-import { loginParameterized, logout } from '../../commonFunction/loginFunctions.js'
-import { contact } from '../../commonFunction/contactFunction.js'
+import { modularLoginFunction, logout } from '../../utilities/loginFunctions.js'
+import { contact } from '../../utilities/contactFunction.js'
 import testData from '../../fixtures/testData.json'
 import contactUsFormData from '../../fixtures/contactUsFormData.json'
+import contactPage from '../../pageObject/contactPage.json'
 
 describe('Contact Us Test Suites', () => {
     beforeEach(() => {
         cy.log('**Test Case Started**');
-        loginParameterized({ useremail: testData.validemail, userpassword: testData.password });
+        modularLoginFunction({ useremail: testData.validemail, userpassword: testData.password });
     });
     it('Contact Form', () => {
-        // cy.log('test case for contact function')  
-        // contact();  //File upload not working in contact() function, same is working in the below test case.
-        cy.get(contactPage.contactButton).click()
-        cy.get(contactPage.name).type(contactUsFormData.name)
-        cy.get(contactPage.email).type(contactUsFormData.email)
-        cy.get(contactPage.subject).type(contactUsFormData.subject)
-        cy.get(contactPage.message).type(contactUsFormData.message)
-        cy.get(contactPage.uploadfileButton).attachFile('samplefile.txt'); //uploading file from fixtures folder
-        cy.get(contactPage.submitButton).click()
-        cy.on('window:confirm', (confirmText) => {
-            // Assertion confirm text
-            expect(confirmText).to.eq(contactUsFormData.confirmText)
-
-            // Automatically clicks 'OK' with true and 'Cancel' with false
-            return true
-        });
-        cy.get(contactPage.successMessage).should('have.text', contactUsFormData.contactFormSuccessMessage)
+        //cy.log('test case for contact function') 
+        //File upload not working in contact() function, same is working in the below test case. 
+        contact();  
     });
     afterEach(() => {
         logout();
