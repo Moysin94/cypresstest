ac4936728d42a3ccaf3d6617e1a33db0cca10975
Moysin94 <shekhmoysin786@gmail.com>
2025-06-01 18:33:52 +0530

    loginDDT test case updated

--- cypress/e2e/automationExercise/loginTestWithDDT.cy.js
+++ cypress/e2e/automationExercise/loginTestWithDDT.cy.js
@@ -3,9 +3,9 @@ import {loginDDTData} from '../../fixtures/loginDDTTestData.js'
 import { modularLoginFunction, logout } from '../../utilities/loginFunctions.js'
 
 describe('login test suite', ()=>{
-     loginDDTData.forEach((loginDDTData) => {
+     loginDDTData.forEach((data) => {
 it('login test with ddt test data', () => {
-    modularLoginFunction({userEmail:loginDDTData.userEmail, userPassword: loginDDTData.userPassword});
+    modularLoginFunction({userEmail:data.userEmail, userPassword: data.userPassword});
     logout();
 });
 })
